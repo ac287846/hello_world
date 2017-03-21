d8e48bacdc6b8ee0bfbb10b030036f60517934ad
Austin Chambers <austinchambers302977@gmail.com>
2017-03-21 01:52:01 -0700

    Added more to the code

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
 // Javascript; Austin Chambers
-
-for(var something=0;something<10;something++){
-  console.log("Hello World");
+function hello(){
+  for(var something=0;something<10;something++){
+    console.log("Hello World");
+  }
 }
+
+function hi(){
+ console.log("Hi there!"); 
+}
+
+hi();
